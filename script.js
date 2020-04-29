3a8403adbb33f7f756a4a592dbf49d22d3c1b075
landonisonline <53879728+Landonisonline@users.noreply.github.com>
2020-04-29 02:41:36 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function colorMode(myColor) {
+var choosenColor = myColor;
+document.body.style.backgroundColor = choosenColor;
+document.body.style.color = "white";
+document.body.style.transition= "background-color 1s, color 0.2s";
+}
+
+
+
+
+
+
+// backup code
+
+//function darkmode() {
+//document.body.style.backgroundColor = "#3b3a3a";
+//document.body.style.color = "white";
+//document.body.style.transition= "background-color 1s, color 0.2s";
+//}
