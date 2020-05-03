639380d9603a231baa3fc99dc066c1eb9e1f0128
landonisonline <53879728+Landonisonline@users.noreply.github.com>
2020-05-02 20:57:12 -0500

    commit

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 
-
-function colorMode(myColor) {
-var choosenColor = myColor;
-document.body.style.backgroundColor = choosenColor;
-document.body.style.color = "white";
-document.body.style.transition= "background-color 1s, color 0.2s";
+function colorMode(colorPass) {
+    var changeTo = colorPass;
+    document.body.style.backgroundColor = `${changeTo}`;
+    document.body.style.transition = "background-color 1s";
 }
-
-
-
-
-
-
-// backup code
-
-//function darkmode() {
-//document.body.style.backgroundColor = "#3b3a3a";
-//document.body.style.color = "white";
-//document.body.style.transition= "background-color 1s, color 0.2s";
-//}
